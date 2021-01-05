074669e2b58c4ab7977b6311e89e4053d231b629
Clemens Damke <cdamke@mail.upb.de>
2021-01-05 18:28:39 +0100

    added tensorboard logging

--- run_board.sh
+++ run_board.sh
@@ -2,10 +2,13 @@
 
 USER="-u $(id -u):$(id -g)"
 
-LOGDIR="/${1:-logs}"
+LOGDIR="/app/${1:-logs}"
 
 if [ -z "$GRASPE_CONTAINER_NAME" ]; then
 	GRASPE_CONTAINER_NAME="graspe"
 fi
 
+TB_PORT=6006
+TB_URL="http://localhost:$TB_PORT"
+echo "Starting TensorBoard at ${TB_URL} ..."
 docker exec -it $USER $(docker ps -aqf "name=^$GRASPE_CONTAINER_NAME\$") tensorboard --logdir $LOGDIR --bind_all
--- src/graspe/playground.py
+++ src/graspe/playground.py
@@ -4,6 +4,7 @@ import funcy as fy
 import networkx as nx
 import numpy as np
 import collections
+from datetime import datetime
 
 import graspe.datasets.synthetic.datasets as syn
 import graspe.preprocessing.utils as enc_utils
@@ -17,6 +18,9 @@ import graspe.models.gnn as gnn
 
 # -%% codecell
 
+def time_str():
+  return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
 def experiment(provider, model):
   enc = fy.first(provider.find_compatible_encoding(
     model.input_encodings, model.output_encodings))
@@ -24,24 +28,38 @@ def experiment(provider, model):
   m = model(
     in_enc=in_enc, out_enc=out_enc,
     in_meta=provider.in_meta, out_meta=provider.out_meta,
-    conv_layer_units=[32, 32, 32], fc_layer_units=[32, 32, 1],
-    activation="sigmoid", inner_activation="relu")
+    conv_layer_units=[32, 32, 32],
+    att_conv_layer_units=[32, 32, 1],
+    fc_layer_units=[32, 32, 1],
+    activation="sigmoid", inner_activation="relu",
+    # att_conv_activation="relu",
+    pooling="softmax")
   if provider.dataset_size < 10:
     ds_train = provider.get(enc)
     ds_val, ds_test = ds_train, ds_train
+    targets = provider.dataset[1]
   else:
     ds_train, ds_val, ds_test = provider.get_split(
       enc, config=dict(batch_size_limit=228),
       outer_idx=5)
+    targets = provider.get_test_split()[1]
 
   opt = keras.optimizers.Adam(0.0001)
   m.compile(
     optimizer=opt, loss="binary_crossentropy", metrics=["binary_accuracy"])
-  m.fit(ds_train, validation_data=ds_val, epochs=5000, verbose=2)
+
+  t = time_str()
+  log_dir = f"../logs/{t}_{m.name}_{provider.name}/"
+  tb = keras.callbacks.TensorBoard(
+    log_dir=log_dir)
+
+  m.fit(ds_train, validation_data=ds_val,
+    epochs=5000, verbose=2,
+    callbacks=[tb])
   m.evaluate(ds_test)
-  # return m.predict(ds_test), provider.get_test_split()[1]
+  print(m.predict(ds_test), targets)
 
 
 provider = syn.triangle_classification_dataset()
-model = gnn.GIN
+model = gnn.WL2GNN
 experiment(provider, model)
